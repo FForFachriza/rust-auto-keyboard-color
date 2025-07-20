2b28a1bc86e520cdc3bafb198949551b55877fa8
fforfachriza <dimasavrian346@gmail.com>
2025-07-21 00:18:35 +0700

    refactor: refactor function to apply color

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,14 @@ use std::process::Command;
 fn main() {
     // Get File Confignya dulu
     // TODO: DIbikin toml maybe?
-    let hex_file = File::open("/home/fforfachriza/.local/state/quickshell/user/generated/color.txt");
+    let path = "/home/fforfachriza/.local/state/quickshell/user/generated/color.txt";
+    apply_color(path);
+
+    println!("Hello, world! ");
+}
+
+fn apply_color(path: &str) -> Result<(), String> {
+    let hex_file = File::open(path);
     let mut hex_codes: String = String::new();
 
     match hex_file {
@@ -20,6 +27,11 @@ fn main() {
         }
     }
 
+    // Check if that code is actually hex, do u want to break ur keyboard dawg?
+    if hex_codes.len() != 6 || !hex_codes.chars().all(|c| c.is_ascii_hexdigit()) {
+        return Err(format!("Invalid hex color: {}", hex_codes));
+    }
+
     let command = Command::new("asusctl")
         .arg("aura")
         .arg("static")
@@ -31,8 +43,8 @@ fn main() {
     if command.status.success() {
         println!("Keyboard color changed successfully!");
     } else {
-        eprintln!("Error: {}", String::from_utf8_lossy(&output.stderr));
+        eprintln!("Error: {}", String::from_utf8_lossy(&command.stderr));
     }
 
-    println!("Hello, world! ");
+    Ok(())
 }
