b71e3aa550fc27a8e0f27a78bc148eaa4db388f5
fforfachriza <dimasavrian346@gmail.com>
2025-07-21 00:31:04 +0700

    feat: using watcher to watching the file changes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::process::Command;
+use notify::{ErrorKind, Event, RecursiveMode, Result, Watcher};
+use std::{path::Path, sync::mpsc};
 
-fn main() {
+fn main()  -> Result<()> {
     // Get File Confignya dulu
     // TODO: DIbikin toml maybe?
     let path = "/home/fforfachriza/.local/state/quickshell/user/generated/color.txt";
-    apply_color(path);
 
-    println!("Hello, world! ");
+    // Watcher
+    let (tx, rx) = mpsc::channel::<Result<Event>>();
+    let mut watcher = notify::recommended_watcher(tx)?;
+    watcher.watch(Path::new(path), RecursiveMode::Recursive)?;
+
+    println!("Watching for changes to {}...", path);
+
+    for res in rx {
+        match res {
+            Ok(event) => {
+                if let notify::EventKind::Modify(_) = event.kind {
+                    println!("\nFile changed! Applying new color...");
+                    if let Err(e) = apply_color(path) {
+                        eprintln!("Error: {}", e);
+                    }
+                }
+            }
+            Err(e) => eprintln!("Watch error: {:?}", e),
+        }
+    }
+
+
+    Ok(())
 }
 
-fn apply_color(path: &str) -> Result<(), String> {
+fn apply_color(path: &str) -> Result<()> {
     let hex_file = File::open(path);
     let mut hex_codes: String = String::new();
 
@@ -19,7 +42,7 @@ fn apply_color(path: &str) -> Result<(), String> {
         Ok(file) => {
             let reader = BufReader::new(file);
             for line in reader.lines() {
-                hex_codes = line.unwrap().replace("#", "");
+                hex_codes = line?.replace("#", "");
             }
         }
         Err(err) => {
@@ -29,7 +52,7 @@ fn apply_color(path: &str) -> Result<(), String> {
 
     // Check if that code is actually hex, do u want to break ur keyboard dawg?
     if hex_codes.len() != 6 || !hex_codes.chars().all(|c| c.is_ascii_hexdigit()) {
-        return Err(format!("Invalid hex color: {}", hex_codes));
+        return Err(notify::Error::generic("Invalid Hex COlor"));
     }
 
     let command = Command::new("asusctl")
